fbdce248b8b4d0a0cfe18c0eeabdd88ec4e4611a
Alexander Leal <computerlanguage5280@gmail.com>
2025-06-10 10:49:13 -0500

    completed Exercise 3 with NorthWind DB

--- NorthwindProducts/src/main/java/Main.java
+++ NorthwindProducts/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
@@ -11,18 +12,65 @@ public class Main {
         String username = "root";
         String password = "yearup2025";
         String url = "jdbc:mysql://localhost:3306/northwind";
-        String query = "SELECT productname FROM products;";
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery(query);
+        Scanner scanner = new Scanner(System.in);
 
-            while (results.next()) {
-                String productName = results.getString("productname");
-                System.out.println(productName);
+        boolean start = true;
+        System.out.println("What do you want to do?");
+        System.out.println("1) Display all products");
+        System.out.println("2) Display all customers");
+        System.out.println("0) Exit");
+
+        while (start) {
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("Displaying all products...");
+                    String query = "SELECT productname, productid, unitprice, unitsinstock FROM products;";
+                    try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                            Statement statement = connection.createStatement();
+                            ResultSet results = statement.executeQuery(query);
+
+                            while (results.next()) {
+                                String productName = results.getString("productname");
+                                int productId = results.getInt("productid");
+                                double unitPrice = results.getDouble("unitprice");
+                                int unitsInStock = results.getInt("unitsinstock");
+                                System.out.printf("\nProduct ID " + productId + " \n Product Name: " + productName + "\n Unit Price: $" + unitPrice + "\n Units In Stock: " + unitsInStock + "\n-------------------");
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                    }
+                    break;
+                case "2":
+                    System.out.println("Displaying all customers...");
+                    String query2 = "SELECT ContactName, CompanyName, City, Country, Phone FROM customers;";
+
+                    try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                        Statement statement = connection.createStatement();
+                        ResultSet results = statement.executeQuery(query2);
+
+                        while (results.next()) {
+                            String contactName = results.getString("ContactName");
+                            String companyName = results.getString("CompanyName");
+                            String city = results.getString("City");
+                            String country = results.getString("Country");
+                            String phoneNumber = results.getString("Phone");
+                            System.out.printf("\nContact Name " + contactName + " \n Company Name: " + companyName + "\n City: " + city + "\n Country: " + country + "" + "\n Phone " + phoneNumber + "\n-------------------");
+                        }
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case "0":
+                    System.out.println("0) Exiting...");
+                    start = false;
+                    break;
+                default:
+                    System.out.println("Choose a valid input.");
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
+        scanner.close();
     }
 }
